18863d46ad8ea57e9970f5769d761dbff2208616
yorlysoro <yorlysoro@gmail.com>
2026-01-29 20:45:16 -0400

    [PDF][ADD] finishing chapter 1 in pdf golang book

---
+++ books/the_go_programming_language/chapter_01/fetch/main.go
@@ -0,0 +1,26 @@
+// fetch prints the content found at a URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			continue
+		}
+		b, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			continue
+		}
+		fmt.Printf("%s", b)
+	}
+}
